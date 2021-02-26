7aa86a41f598387caa79165efb7e2eba273200bf
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-26 12:12:59 -0600

    added hooks

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { useState } from "react";
 
 const Home = () => {
+    // let name = "Gabriel";
+    {/* this is array destructuring (line below) */}
+    const [name, setName] = useState("Gabe"); {/* useState is the hook */}
+    const [age, setAge] = useState(25);
+
+    const handlePostChirpClick = () => {
+        setName("Luigi");
+        setAge(30);
+    }
+
     return (
         <div className="home">
-            <h1>Welcome to Chirper</h1>
-            <h3>Chirp it Out</h3>
-            <h5>Did you get what I was trying to do there?</h5>
+            <div className="welcomeMessage">
+                {/* remove word "homepage" once push to production (line below) */}
+                <h1>Welcome to Chirper (homepage)</h1> 
+                <h3>Chirp it Out</h3>
+                <h5>Did you get what I was trying to do there?</h5>
+            </div>
+            <p>{ name } is { age } years old.</p>
+            <button onClick={handlePostChirpClick}>Post Chirp</button>
+
+            {/* Put array/list of Chirps HERE */}
         </div>
     );
 };
