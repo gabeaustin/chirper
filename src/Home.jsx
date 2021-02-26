489b1d40973ff0d9137f911c748c452ef319d075
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-26 12:24:25 -0600

    added 3 chirps that should show on the screen when page loads

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { useState } from "react";
 
 const Home = () => {
-    // let name = "Gabriel";
+    const [chirps, setChirps] = useState([
+        { body: "Lorem ipsum vaccine the new normal virtual happy hour Blursday.", author: "birthday_boy", timestamp: "", id: 1 },
+        { body: "Netflix self care home haircut droplet Lysol spray.", author: "netflixChill", timestamp: "", id: 2 },
+        { body: "Lorem ipsum Queen's Gambit sanitizer Dr. Fauci ballot.", author: "fauci-for-Pres", timestamp: "", id: 3 }
+    ]);
+
     {/* this is array destructuring (line below) */}
     const [name, setName] = useState("Gabe"); {/* useState is the hook */}
     const [age, setAge] = useState(25);
@@ -15,13 +20,12 @@ const Home = () => {
     return (
         <div className="home">
             <div className="welcomeMessage">
-                {/* remove word "homepage" once push to production (line below) */}
-                <h1>Welcome to Chirper (homepage)</h1> 
-                <h3>Chirp it Out</h3>
-                <h5>Did you get what I was trying to do there?</h5>
+                <h1>Welcome to Chirper</h1> 
             </div>
-            <p>{ name } is { age } years old.</p>
+            
             <button onClick={handlePostChirpClick}>Post Chirp</button>
+            
+            <p>{ name } is { age } years old.</p>
 
             {/* Put array/list of Chirps HERE */}
         </div>
